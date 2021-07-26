1efeaa0143aa30a2e0d3f5227dde0a796485ee23
rishiak007 <79655014+rishiak007@users.noreply.github.com>
2021-07-26 19:55:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,16 @@ app.config['MYSQL_USER'] = 'admin'
 app.config['MYSQL_PASSWORD'] = 'rishi123'
 app.config['MYSQL_DB'] = 'reg'
 
+db = mysql.connect(
+    host = "database-2.cshux5aaxeaw.us-east-1.rds.amazonaws.com",
+    user = "admin",
+    passwd = "rishi123"
+)
+cursor = db.cursor()
+cursor.execute("CREATE DATABASE reg")
+cursor.execute("USE reg")
+cursor.execute("CREATE TABLE information (name VARCHAR(150), age INT(3), email VARCHAR(150), phone VARCHAR(150))") 
+
 mysql = MySQL(app)
 
 
